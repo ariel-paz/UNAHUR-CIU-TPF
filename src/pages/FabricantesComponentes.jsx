cf6968612041e2254baf3b9aa5826b36385d71ad
MaxiGomez5431 <mamitgomez@gmail.com>
2024-11-24 17:52:03 -0300

    Error en Home solucionado

--- src/pages/FabricantesComponentes.jsx
+++ src/pages/FabricantesComponentes.jsx
@@ -1,5 +1,4 @@
 import CatalogoElementos from '../components/CatalogoElementos';
-import SectionContainer from '../components/SectionContainer';
 import { API_ROUTES } from '../src_config_api';
 
 export default function FabricantesComponentes() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -241,7 +241,7 @@ function Home() {
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6566.905339887034!2d-58.63710146096076!3d-34.617999347060284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb8d055e548bf%3A0x738f85ea67aeabe5!2sUniversidad%20Nacional%20de%20Hurlingham!5e0!3m2!1ses!2sar!4v1732478622161!5m2!1ses!2sar" 
               width="400" height="300"
-              allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+              allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
             />
             </div>
           </div>
